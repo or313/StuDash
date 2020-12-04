32e490305e97324427fcc5e37ee6893ed5eeeb87
Or Barel <or.b@spot.im>
2020-12-04 14:27:15 +0200

    Main app

--- StuDash/settings.py
+++ StuDash/settings.py
@@ -1,108 +1,140 @@
+import os
+import warnings
+from django.utils.translation import ugettext_lazy as _
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
+warnings.simplefilter("error", DeprecationWarning)
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+CONTENT_DIR = os.path.join(BASE_DIR, "content")
 
+SECRET_KEY = "NhfTvayqggTBPswCXXhWaN69HuglgZIkM"
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mgeep2a-d#admh5gsfa*htrygi-y$755yegp@s3nu4*$j=_ge0'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 ALLOWED_HOSTS = []
 
-
-# Application definition
+SITE_ID = 1
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.sites",
+    # Vendor apps
+    "bootstrap4",
+    # Application apps
+    "main",
+    # 'accounts',
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'StuDash.urls'
+ROOT_URLCONF = "StuDash.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            os.path.join(CONTENT_DIR, "templates"),
+        ],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'StuDash.wsgi.application'
+WSGI_APPLICATION = "StuDash.wsgi.application"
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+EMAIL_HOST = ""
+EMAIL_HOST_USER = "apikey"
+DEFAULT_FROM_EMAIL = "orteach123@gmail.com"
+EMAIL_HOST_PASSWORD = ""
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
+DISABLE_USERNAME = False
+LOGIN_VIA_EMAIL = True
+LOGIN_VIA_EMAIL_OR_USERNAME = False
+LOGIN_REDIRECT_URL = "home"
+# LOGIN_URL = "accounts:log_in"
+USE_REMEMBER_ME = True
 
-# Internationalization
-# https://docs.djangoproject.com/en/3.1/topics/i18n/
+RESTORE_PASSWORD_VIA_EMAIL_OR_USERNAME = False
 
-LANGUAGE_CODE = 'en-us'
+SIGN_UP_FIELDS = [
+    "username",
+    "first_name",
+    "last_name",
+    "email",
+    "password1",
+    "password2",
+]
+if DISABLE_USERNAME:
+    SIGN_UP_FIELDS = ["first_name", "last_name", "email", "password1", "password2"]
 
-TIME_ZONE = 'UTC'
+MESSAGE_STORAGE = "django.contrib.messages.storage.cookie.CookieStorage"
 
 USE_I18N = True
-
 USE_L10N = True
+LANGUAGE_CODE = "en"
+LANGUAGES = [
+    ("en", _("English")),
+]
 
+TIME_ZONE = "UTC"
 USE_TZ = True
 
+STATIC_ROOT = os.path.join(CONTENT_DIR, "static")
+STATIC_URL = "/static/"
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
+MEDIA_ROOT = os.path.join(CONTENT_DIR, "media")
+MEDIA_URL = "/media/"
 
-STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(CONTENT_DIR, "assets"),
+]
+
+LOCALE_PATHS = [os.path.join(CONTENT_DIR, "locale")]
--- StuDash/urls.py
+++ StuDash/urls.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path
+from django.conf.urls.static import static
+
+from main.views import IndexPageView
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", IndexPageView.as_view(), name="home"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

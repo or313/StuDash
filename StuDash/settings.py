d168111073c48e4dac7db2eb5fea001d5d7f5df0
Or Barel <or.b@spot.im>
2020-12-04 14:26:32 +0200

    Removing StuDash_home
    Remove vscode from gitignore

--- StuDash/settings.py
+++ StuDash/settings.py
@@ -5,12 +5,12 @@ from os.path import dirname
 from pathlib import Path
 
 
-warnings.simplefilter('error', DeprecationWarning)
+warnings.simplefilter("error", DeprecationWarning)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-CONTENT_DIR = os.path.join(BASE_DIR, 'content')
+CONTENT_DIR = os.path.join(BASE_DIR, "content")
 
-SECRET_KEY = 'NhfTvayqggTBPswCXXhWaN69HuglgZIkM'
+SECRET_KEY = "NhfTvayqggTBPswCXXhWaN69HuglgZIkM"
 
 DEBUG = True
 ALLOWED_HOSTS = []
@@ -18,121 +18,124 @@ ALLOWED_HOSTS = []
 SITE_ID = 1
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.sites',
-
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.sites",
     # Vendor apps
-    'bootstrap4',
-
+    "bootstrap4",
     # Application apps
-    'main',
-    'accounts',
+    "main",
+    # 'accounts',
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'StuDash.urls'
+ROOT_URLCONF = "StuDash.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(CONTENT_DIR, 'templates'),
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            os.path.join(CONTENT_DIR, "templates"),
         ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'StuDash.wsgi.application'
+WSGI_APPLICATION = "StuDash.wsgi.application"
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'apikey'
-DEFAULT_FROM_EMAIL = 'orteach123@gmail.com'
-EMAIL_HOST_PASSWORD = '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'
+EMAIL_HOST = ""
+EMAIL_HOST_USER = "apikey"
+DEFAULT_FROM_EMAIL = "orteach123@gmail.com"
+EMAIL_HOST_PASSWORD = ""
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
 DISABLE_USERNAME = False
 LOGIN_VIA_EMAIL = True
 LOGIN_VIA_EMAIL_OR_USERNAME = False
-LOGIN_REDIRECT_URL = 'home'
-LOGIN_URL = 'accounts:log_in'
+LOGIN_REDIRECT_URL = "home"
+# LOGIN_URL = "accounts:log_in"
 USE_REMEMBER_ME = True
 
 RESTORE_PASSWORD_VIA_EMAIL_OR_USERNAME = False
 
-SIGN_UP_FIELDS = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
+SIGN_UP_FIELDS = [
+    "username",
+    "first_name",
+    "last_name",
+    "email",
+    "password1",
+    "password2",
+]
 if DISABLE_USERNAME:
-    SIGN_UP_FIELDS = ['first_name', 'last_name', 'email', 'password1', 'password2']
+    SIGN_UP_FIELDS = ["first_name", "last_name", "email", "password1", "password2"]
 
-MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
+MESSAGE_STORAGE = "django.contrib.messages.storage.cookie.CookieStorage"
 
 USE_I18N = True
 USE_L10N = True
-LANGUAGE_CODE = 'en'
+LANGUAGE_CODE = "en"
 LANGUAGES = [
-    ('en', _('English')),
+    ("en", _("English")),
 ]
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 USE_TZ = True
 
-STATIC_ROOT = os.path.join(CONTENT_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(CONTENT_DIR, "static")
+STATIC_URL = "/static/"
 
-MEDIA_ROOT = os.path.join(CONTENT_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(CONTENT_DIR, "media")
+MEDIA_URL = "/media/"
 
 STATICFILES_DIRS = [
-    os.path.join(CONTENT_DIR, 'assets'),
+    os.path.join(CONTENT_DIR, "assets"),
 ]
 
-LOCALE_PATHS = [
-    os.path.join(CONTENT_DIR, 'locale')
-]
+LOCALE_PATHS = [os.path.join(CONTENT_DIR, "locale")]
--- StuDash/urls.py
+++ StuDash/urls.py
@@ -1,4 +1,3 @@
-
 from django.contrib import admin
 from django.conf import settings
 from django.urls import path
@@ -9,11 +8,9 @@ from django.conf.urls.static import static
 from main.views import IndexPageView
 
 
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-    path('', IndexPageView.as_view(), name='home'),
+    path("admin/", admin.site.urls),
+    path("", IndexPageView.as_view(), name="home"),
 ]
 
 if settings.DEBUG:

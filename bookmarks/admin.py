b5aede70a8ca30798207cac3f33350ad8f7b4f69
Noa R <noa.ramon01@gmail.com>
2020-12-12 21:50:12 +0200

    admin.py modified

--- bookmarks/admin.py
+++ bookmarks/admin.py
@@ -3,7 +3,7 @@ from django.apps import apps
 # Register your models here.
 models = apps.get_models()
 for model in models:
-    try:       
-        admin.site.register(model) 
+    try:
+        admin.site.register(model)
     except admin.sites.AlreadyRegistered:
         pass

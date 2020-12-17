dcdc85a353864d312c927cfccaa4fea6ed512f39
Noa R <noa.ramon01@gmail.com>
2020-12-17 08:56:19 +0200

    Bookmarks class modified due to the  new Course class

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-# course= models.ForeignKey(Course) will be added to this class
-# after class Course will be created by other team member
 # descripition- Users are related to certain courses.
 # for each course, the user is able to create relevant bookmarks
 # each bookmark stores url, name and last visit date in order to enable potential future filtering
@@ -9,6 +7,7 @@ from django.contrib.auth.models import User
 
 class Bookmark (models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    course= models.ForeignKey(Course, on_delete=models.CASCADE)
     url = models.URLField(max_length=200)
     urlname = models.CharField(max_length=30)
     last_view_date = models.DateTimeField(auto_now=True, null=True)

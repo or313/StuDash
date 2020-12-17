e3f440aba5d14efd67fc5ecf525bd962452c596e
Noa R <noa.ramon01@gmail.com>
2020-12-17 09:04:39 +0200

    flake8 changes

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User, Course
 
 class Bookmark (models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    course= models.ForeignKey(Course, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
     url = models.URLField(max_length=200)
     urlname = models.CharField(max_length=30)
     last_view_date = models.DateTimeField(auto_now=True, null=True)

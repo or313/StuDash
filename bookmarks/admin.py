f9125885df571b571a7a82f05662f3cad2995a21
Noa R <noa.ramon01@gmail.com>
2020-12-20 18:54:25 +0200

    Data model changes due to code review

--- bookmarks/admin.py
+++ bookmarks/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.apps import apps
+
 # Register your models here.
 models = apps.get_models()
+
 for model in models:
     try:
         admin.site.register(model)
--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import User, Course
+from django.conf import settings
+from django.contrib.auth.models import User
+from grades.models import Course
 # descripition- Users are related to certain courses.
 # for each course, the user is able to create relevant bookmarks
 # each bookmark stores url, name and last visit date in order to enable potential future filtering
@@ -7,18 +9,10 @@ from django.contrib.auth.models import User, Course
 
 class Bookmark (models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, default='SOME STRING')
     url = models.URLField(max_length=200)
     urlname = models.CharField(max_length=30)
     last_view_date = models.DateTimeField(auto_now=True, null=True)
-
-    @classmethod
-    def addbookmark(cls, current_user, new_link):
-        new_link.user.add(current_user)
-
-    @classmethod
-    def removebookmark(cls, current_user, remove_link):
-        remove_link.user.remove(current_user)
 
     def __str__(self):
         return self.urlname

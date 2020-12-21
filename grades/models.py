5ede52c11f6fd25ba699c238708941fed752adc4
meitals91 <meitals91@gmail.com>
2020-12-21 22:24:37 +0200

    Adding def str to course class

--- grades/models.py
+++ grades/models.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
-
 class Course (models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
                              related_name='%(class)s_student', null=True, blank=True)
@@ -16,27 +15,47 @@ class Course (models.Model):
         ]
     )
 
+    def __str__(self):
+        return self.course_name
+
+    @classmethod
+    def addCourse(cls, user, course_name, credinitials):
+        newCourse = Course()
+        newCourse.user = user
+        newCourse.course_name = course_name
+        newCourse.credinitials = credinitials
+        newCourse.save()
+        return newCourse
+
+    @classmethod
+    def removeCourse(cls, courseID):
+        Course.objects.filter(id=courseID).delete()
+        pass
+
+
+class Semester(models.TextChoices):
+    Winter = 'A',
+    Spring = 'B',
+    Summer = 'C'
+
 
 class Grade(models.Model):
-    SEMESTER_NAME = (
-        ('A', 'Winter'),
-        ('B', 'Spring'),
-        ('C', 'Summer'),
-    )
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
-                             related_name='%(class)s_student', null=True, blank=True)
-    course = models.ForeignKey(Course, on_delete=models.RESTRICT, null=True, blank=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
     grade = models.IntegerField(default=0)
-    semester = models.CharField(max_length=1, choices=SEMESTER_NAME)
+    semester = models.CharField(max_length=1, choices=Semester.choices)
     year = models.IntegerField(default=0)
 
     @classmethod
-    def addGradeUserCourse(cls, user, courseID, grade, semester, year):
-        newGrade = Grade.object.create()
-        newGrade.user = user
+    def addGradeUserCourse(cls, courseID, grade, semester, year):
+        newGrade = Grade()
         newGrade.course = courseID
         newGrade.grade = grade
         newGrade.semester = semester
         newGrade.year = year
+        newGrade.save()
         return newGrade
 
+    @classmethod
+    def removeGrade(cls, gradeID):
+        Grade.objects.filter(id=gradeID).delete()
+        pass
--- grades/tests.py
+++ grades/tests.py
@@ -1,3 +1,51 @@
-# from django.test import TestCase
+import pytest
+from grades.models import Grade, Course
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+@pytest.mark.django_db
+class TestGrade:
+    def test_addGradeUserCourse(cls):
+        user3 = User.objects.create_user(username="idan", password="123456")
+        c = Course.addCourse(user=user3, course_name="NLP", credinitials="3")
+        Grade.addGradeUserCourse(c, 90, 'A', 2021)
+        out = Grade.objects.filter(semester='A')
+        assert list(out.values_list('course', 'grade', 'semester', 'year')) == [
+            (3, 90, 'A', 2021)
+        ]
+
+    def test_removeGrade(cls):
+        user3 = User.objects.create_user(username="idan", password="123456")
+        c = Course.addCourse(user=user3, course_name="NLP", credinitials="3")
+        Grade.addGradeUserCourse(c, 50, 'B', 2020)
+        Grade.removeGrade(2)
+        out = Grade.objects.all()
+        assert list(out.values_list('course', 'grade', 'semester', 'year')) == [
+            (3, 50, 'B', 2020),
+        ]
+
+
+@pytest.mark.django_db
+class TestCourse:
+    def test_addCourse(cls):
+        user4 = User.objects.create_user(username="noa", password="123456")
+        Course.addCourse(user=user4, course_name="java", credinitials="3")
+        out = Course.objects.filter(credinitials='3')
+        assert list(out.values_list('user', 'course_name', 'credinitials')) == [
+            (1, 'course3', 3),
+            (2, 'course2', 3),
+            (3, 'java', 3),
+        ]
+
+    def test_removeCourse(cls):
+        user4 = User.objects.create_user(username="noa", password="123456")
+        Course.addCourse(user=user4, course_name="java", credinitials="3")
+        Course.removeCourse(3)
+        Course.removeCourse(4)
+        Course.removeCourse(5)
+        out = Course.objects.all()
+        assert list(out.values_list('user', 'course_name', 'credinitials')) == [
+            (1, 'course3', 3),
+            (2, 'course2', 3),
+
+        ]

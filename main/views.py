daafcb1875d665d52392f250f90544b0261b85d2
Or Barel <or.b@spot.im>
2020-12-24 19:21:04 +0200

    Auth guard over pages under base page

--- main/views.py
+++ main/views.py
@@ -1,5 +1,9 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
+
+@method_decorator(login_required(login_url='/accounts/log-in'), name='dispatch')
 class IndexPageView(TemplateView):
-    template_name = 'main/index.html'
+    template_name = 'layouts/default/page.html'

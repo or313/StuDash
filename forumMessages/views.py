663688a893e543ce935477f34fb4fb1f0934a394
Gal-f <galfroind@gmail.com>
2021-01-06 17:37:21 +0200

    Creating UI logic in a views file
    Adding UI logic to views.py file,
    for both add message and add category forms.

--- forumMessages/views.py
+++ forumMessages/views.py
@@ -1,4 +1,25 @@
-# from django.shortcuts import render
-# Remove comment when implementing views
+from django.shortcuts import render, redirect
+from .models import Message, Category
+from .forms import MessageForm, CategoryForm
 
-# Create your views here.
+def ViewMessages(request):
+    messages = Message.objects.order_by('-date')
+    if request.method == "POST":
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(ViewMessages)
+    else:
+        form = MessageForm()
+    return render(request, 'forumMessages/messagelist.html', {'messages': messages, 'form': form,})
+
+def ViewCategories(request):
+    categories = Category.objects.order_by('-id')
+    if request.method == "POST":
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(ViewCategories)
+    else:
+        form = CategoryForm()
+    return render(request, 'forumMessages/categorylist.html', {'categories': categories, 'form': form,})

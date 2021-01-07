4d190d13976735c048242fb8a61c6d310af61067
Gal-f <galfroind@gmail.com>
2021-01-08 01:51:02 +0200

    Rebases on upstream/main + fixed several Newlines

--- forumMessages/forms.py
+++ forumMessages/forms.py
@@ -1,10 +1,12 @@
 from django import forms
 from .models import Message, Category
 
+
 class MessageForm(forms.ModelForm):
     class Meta:
         model = Message
         fields = ('user', 'text', 'date', 'categories')
+
 
 class CategoryForm(forms.ModelForm):
     class Meta:
